fc69378f7e4854bcc950fccee5bc9b76870d51b3
WAZEEMA AAMIR <161026876+WazeemaAamir@users.noreply.github.com>
2025-10-25 00:33:11 +0500

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,9 @@
+// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true, // Build ke waqt eslint errors ignore ho jayenge
+  },
+};
+
+module.exports = nextConfig;
