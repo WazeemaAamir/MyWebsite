d2f917d665de8eee28c0963ae81c7cd7af9c8675
WAZEEMA AAMIR <161026876+WazeemaAamir@users.noreply.github.com>
2025-10-25 01:15:44 +0500

    Delete next.config.ts

--- next.config.ts
+++
@@ -1,9 +0,0 @@
-// next.config.js
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true, // Build ke waqt eslint errors ignore ho jayenge
-  },
-};
-
-module.exports = nextConfig;
