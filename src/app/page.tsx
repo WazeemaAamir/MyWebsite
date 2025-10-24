57758b6177160154f1f076d257caa7e2e95dea95
WAZEEMA AAMIR <161026876+WazeemaAamir@users.noreply.github.com>
2025-10-25 01:03:02 +0500

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import Navbar from "@/Components/Navbar";
+import Hero from "@/Components/Hero";
+import Footer from "@/Components/Footer";
+
+export default function HomePage() {
+  return (
+    <div className="min-h-screen flex flex-col bg-gray-50">
+      {/* Navbar */}
+      <Navbar />
+
+      {/* Hero Banner */}
+      <Hero />
+      
+      {/* Footer */}
+      <Footer />
+    </div>
+  );
+}
