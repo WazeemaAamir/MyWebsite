0d9e0e52acf0d82446e6162221ba1067487a1582
WAZEEMA AAMIR <161026876+WazeemaAamir@users.noreply.github.com>
2025-11-04 17:16:28 +0500

    Delete src directory

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Topbar from "@/components/Topbar";
-
-export default function Home() {
-  return (
-    <div className="flex min-h-screen flex-col items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <Topbar />
-    </div>
-  );
-}
